641465d66aacce434d2b1c95aa3e77e8e84ba79a
edsonnaza <edsonnaza@gmail.com>
2024-01-23 21:31:26 -0300

    Final PI finish code

--- src/utils/validateForm.js
+++ src/utils/validateForm.js
@@ -10,39 +10,41 @@ const validateForm =  (formData) => {
     if (/[^a-zA-Z\s]/.test(formData.forename)) {
      // setFormData((prevData) => ({ ...prevData, forenameMessage: 'The name cannot contain symbols.' }));
      errorMessage={...errorMessage,forename:'The name cannot contain symbols.'}
-    
+     return errorMessage
     }
   
     if (/[^a-zA-Z\s]/.test(formData.lastname)) {
-        errorMessage={...errorMessage,forename:'The lastname cannot contain symbols.'}
+        
+        errorMessage={...errorMessage,lastname:'The lastname cannot contain symbols.'}
       //setFormData((prevData) => ({ ...prevData, lastnameMessage: 'The lastname cannot contain symbols.' }));
-     // return false;
+      console.log('lastName:',formData.lastname);
+      return errorMessage;
     }
   
     if (formData.forename === '') {
         errorMessage ={ ...errorMessage, forename:'Name is empty, please write a valid name!'}
      // setFormData((prevData) => ({ ...prevData, forenameMessage: 'Name is empty, please write a valid name!' }));
-     // return false;
+      return errorMessage;
     }
   
     if (formData.lastname === '') {
         errorMessage ={ ...errorMessage, lastname:'Lastname is empty, please write a valid lastname!'}
     //   setFormData((prevData) => ({ ...prevData, lastnameMessage: 'Lastname is empty, please write a valid lastname!' }));
-    //   return false;
+      return errorMessage;
     }
   
     if (formData.nationality === '') {
         errorMessage ={ ...errorMessage, nationality:'Nationality is empty, please write a valid nationality!'}
 
     //   setFormData((prevData) => ({ ...prevData, nationalityMessage: 'Nationality is empty, please write a valid nationality!' }));
-    //   return false;
+      return errorMessage;
     }
   
     if (formData.dob === '') {
         errorMessage ={ ...errorMessage, dob:'Date of birth is empty, please write a valid date!'}
 
     //   setFormData((prevData) => ({ ...prevData, dobMessage: 'Date of birth is empty, please select a valid date!' }));
-    //   return false;
+       return errorMessage;
     }
   
     const regex = /^(?:(?:19|20)\d\d|21[0-9]{2})[-/](0[1-9]|1[0-2])[-/](0[1-9]|[12][0-9]|3[01])$|^([0-2][0-9]|3[01])[-/](0[1-9]|1[0-2])[-/](?:19|20)\d\d$/;
@@ -51,14 +53,14 @@ const validateForm =  (formData) => {
         errorMessage ={ ...errorMessage, dob:'Invalid date format. Use YYYY/MM/DD, DD/MM/YYYY, MM/DD/YYYY o YYYY-MM-DD'}
 
     //   setFormData((prevData) => ({ ...prevData, dobMessage: 'Invalid date format. Use YYYY/MM/DD, DD/MM/YYYY, MM/DD/YYYY o YYYY-MM-DD' }));
-    //   return false;
+      return errorMessage;
     }
   
     if (formData.description === '') {
         errorMessage ={ ...errorMessage, description:'Description is empty, please write a valid description!'}
 
     //   setFormData((prevData) => ({ ...prevData, descriptionMessage: 'Description is empty, please write a valid description!' }));
-    //   return false;
+       return errorMessage;
     }
 
     
@@ -67,7 +69,7 @@ const validateForm =  (formData) => {
         errorMessage ={ ...errorMessage, teams:'Teams is empty, please select at least one team!' }
         
     //   setFormData((prevData) => ({ ...prevData, teamsMessage: 'Teams is empty, please select at least one team!' }));
-    //   return false;
+       return errorMessage;
     }
   
     
