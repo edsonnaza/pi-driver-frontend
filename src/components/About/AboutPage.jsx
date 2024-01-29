dd36f5f50977b496625a8731950d52a417c6da64
edsonnaza <edsonnaza@gmail.com>
2024-01-29 11:00:15 -0300

    AboutPage Sass cofiguration Small devices

--- src/components/About/AboutPage.jsx
+++ src/components/About/AboutPage.jsx
@@ -1,6 +1,11 @@
 import style from './AboutPage.module.scss';
 import { Link } from 'react-router-dom';
  
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub, faWsh } from '@fortawesome/free-brands-svg-icons';
+
+
+ 
 const AboutPage = () =>{
     return (
         <div className={style.content} data-testid="AboutPage">
@@ -10,11 +15,19 @@ const AboutPage = () =>{
 
         <h1 className={style.h1}>Edson Sanchez</h1>
         <div className={style.info}>
-          <p className={style.infLink}><Link to="https://linkedin.com/in/edsonnaza">linkedin.com/in/edsonnaz</Link>a</p>
-          <p className={style.infLink}><Link to="https://github.com/edsonnaza">github.com/edsonnaza</Link>a</p>
-          <p className={style.infLink}><Link to="https://wakatime.com/@edsonnaza">wakatime.com/@edsonnaza</Link>a</p>
-        
-        </div>
+  <p className={style.infLink}>
+  <FontAwesomeIcon icon={faLinkedin}/> 
+    <Link to="https://linkedin.com/in/edsonnaza"> linkedin.com/in/edsonnaz</Link>
+  </p>
+  <p className={style.infLink}>
+  <FontAwesomeIcon icon={faGithub}/> 
+     <Link to="https://github.com/edsonnaza"> github.com/edsonnaza</Link>
+  </p>
+  <p className={style.infLink}>
+  <FontAwesomeIcon icon={faWsh}/> 
+    <Link to="https://wakatime.com/@edsonnaza"> wakatime.com/@edsonnaza</Link>
+  </p>
+</div>
           <p className={style.p}>
             Soy un entusiasta de la programación y disfruto compartiendo conocimientos con la comunidad.
           </p>
--- src/components/WelcomePage/WelcomePage.jsx
+++ src/components/WelcomePage/WelcomePage.jsx
@@ -5,12 +5,12 @@ import {Link} from 'react-router-dom';
 const WelcomePage = ()=>{
 
     return (
-    <div className={style['welcome-page']} data-testid="WelcomePage">
+<div className={style['welcome-page']} data-testid="WelcomePage">
       
- <h1 className={style.titleWelcome}>Welcome to formula one world</h1>
      
 
 <div className={style.buttonContainer}>
+ <h1 className={style.titleWelcome}>Welcome to formula one world</h1>
   <Link to="/home">
     
   <button className={style["mybutton"]} type ="button" name="engineButton" data-testid="EngineButton">
